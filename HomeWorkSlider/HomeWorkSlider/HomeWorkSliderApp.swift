150ae1550021f2b6a9c483eda5bcbce70ab776f0
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-06-02 16:13:58 +0300

    New Project

---
+++ HomeWorkSlider/HomeWorkSlider/HomeWorkSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkSliderApp.swift
+//  HomeWorkSlider
+//
+//  Created by Ваня Науменко on 2.06.22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
