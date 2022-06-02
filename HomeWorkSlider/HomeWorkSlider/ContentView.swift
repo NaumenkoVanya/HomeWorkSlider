2ea32e67995e2b61df6498b0716ff3220875b57c
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-06-02 16:44:02 +0300

    Slider

--- HomeWorkSlider/HomeWorkSlider/ContentView.swift
+++ HomeWorkSlider/HomeWorkSlider/ContentView.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
+// MARK: - ContentView
+
 struct ContentView: View {
+    @State var redComponent: Double = 0.5
+    @State var greenComponent: Double = 0.5
+    @State var blueComponent: Double = 0.5
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Color(red: redComponent, green: greenComponent, blue: blueComponent).padding()
+            Slider(value: $redComponent)
+            Slider(value: $greenComponent)
+            Slider(value: $blueComponent)
+            
+        }
+        .padding(.all, 10.0)
     }
 }
+
+// MARK: - ContentView_Previews
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
